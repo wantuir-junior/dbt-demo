c49cb6d1b2b24eebe1e7a612212dd3108cf8dd05
Wantuir Junior <junior@thesmartcone.com>
2023-09-26 11:20:38 -0400

    customer sources and staging

---
+++ my_project/models/staging/tpch/stg_customer.sql
@@ -0,0 +1,24 @@
+with source as (
+
+    select * from {{ source('tpch', 'customer') }}
+
+),
+
+renamed as (
+
+    select
+
+        c_cust_key as customer_key,
+        c_name as customer_name,
+        c_address as customer_address,
+        c_nationkey as nationkey,
+        c_phone as phone,
+        c_acctbal as account_balance,
+        c_mktsegment as marketing_segment,
+        c_comment as comment
+
+    from source
+
+)
+
+select * from renamed
