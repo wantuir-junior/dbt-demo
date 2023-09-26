57c8fb9d6a0db57705b72432cc3af12784c817c5
Wantuir Junior <junior@thesmartcone.com>
2023-09-26 12:59:44 -0400

    fix nation key column naming

--- my_project/models/staging/tpch/stg_tpch_customer.sql
+++ my_project/models/staging/tpch/stg_tpch_customer.sql
@@ -11,7 +11,7 @@ renamed as (
         c_custkey as customer_key,
         c_name as customer_name,
         c_address as customer_address,
-        c_nationkey as nationkey,
+        c_nationkey as nation_key,
         c_phone as phone,
         c_acctbal as account_balance,
         c_mktsegment as marketing_segment,
